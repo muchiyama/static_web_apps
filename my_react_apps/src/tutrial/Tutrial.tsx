527a154852fb7b2f40b7a04fc8191fa1a6b72172
muchiyama <masatomo.uchiyama@jbs.com>
2020-08-16 23:48:19 +0900

    updated

--- my_react_apps/src/tutrial/Tutrial.tsx
+++ my_react_apps/src/tutrial/Tutrial.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 
 import './Tutrial.css'
-import { FeeClassification, DetailProps } from './TutrialTypes'
+// import { FeeClassification, DetailProps } from './TutrialTypes'
 
 
 
