835fdd3b334aed971db69a21826ff001e18f1037
muchiyama <masatomo.uchiyama@jbs.com>
2020-08-17 00:32:56 +0900

    updated

--- my_react_apps/src/tutrial/Tutrial.tsx
+++ my_react_apps/src/tutrial/Tutrial.tsx
@@ -5,54 +5,65 @@ import './Tutrial.css'
 import { DetailProps } from './TutrialTypes'
 
 class Detail extends React.Component<DetailProps, {}> {
-    render() {
-      return (
-        <div >
-          <div className="classification-name">{this.props.classification.name}</div>
-          <div className="description">{this.props.classification.description}</div>
-          <div className="unit-price">{this.props.classification.unitPrice}</div>
-          <div className="num-people">
-            <select value={this.props.classification.numOfPeople}>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-            </select>
-            <span>名</span>
-          </div>
+  render() {
+    return (
+      <div >
+        <div className="classification-name">{this.props.classification.name}</div>
+        <div className="description">{this.props.classification.description}</div>
+        <div className="unit-price">{this.props.classification.unitPrice}円</div>
+        <div className="num-people">
+          <select value={this.props.classification.numOfPeople}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+          <span>名</span>
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
   
-  class Summary extends React.Component {
-    render() {
-      return (
-        <div>
-          <div className="party">
-            <input type="text" className="party" value="0" />
-            <span>名様</span>
-          </div>
-          <div className="total-amount">
-            <span>合計</span>
-            <input type="text" className="total-amount" value="0" />
-            <span>円</span>
-          </div>
+class Summary extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="party">
+          <input type="text" className="party" value="0" />
+          <span>名様</span>
         </div>
-      );
-    }
+        <div className="total-amount">
+          <span>合計</span>
+          <input type="text" className="total-amount" value="0" />
+          <span>円</span>
+        </div>
+      </div>
+    );
   }
-  
-  class AdmissionFeeCalculator extends React.Component {
-    render() {
-      return (
-        <>
-          <Detail />
-          <Summary />
-        </>
-      );
+}
+
+class AdmissionFeeCalculator extends React.Component {
+  private detail: DetailProps = {
+    classification: {
+      name: "大人",
+      description: "",
+      unitPrice: 1000,
+      numOfPeople: 0,
+      totalPrice: 0
     }
+  };
+
+  render() {
+    return (
+      <>
+        <Detail classification={this.detail.classification} />
+        <Summary />
+      </>
+    );
   }
+}
 
 export default AdmissionFeeCalculator
   
